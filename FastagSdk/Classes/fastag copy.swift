c8b922502e144c49e7f6e93ff58505b637771d38
Madhan-Rkv-10 <madhanrkv10@gmail.com>
2025-09-05 17:08:37 +0530

    update:version

--- FastagSdk/Classes/fastag copy.swift
+++ FastagSdk/Classes/fastag copy.swift
@@ -1,55 +1,55 @@
 
-import UIKit
-import Flutter
-import FlutterPluginRegistrant
+// import UIKit
+// import Flutter
+// import FlutterPluginRegistrant
 
-internal final class InternalFlutterEngine {
-   static let shared = InternalFlutterEngine()
+// internal final class InternalFlutterEngine {
+//    static let shared = InternalFlutterEngine()
 
-   private(set) var flutterEngine: FlutterEngine?
-   private(set) var flutterViewController: UIViewController?
-   private var engineStarted = false
+//    private(set) var flutterEngine: FlutterEngine?
+//    private(set) var flutterViewController: UIViewController?
+//    private var engineStarted = false
 
-   private init() {}
+//    private init() {}
 
-   private let networkMonitor = NetworkMonitor.shared
+//    private let networkMonitor = NetworkMonitor.shared
 
-   // Start the Flutter engine
-   internal func startEngineIfNeeded() {
-       guard !engineStarted else { return }
+//    // Start the Flutter engine
+//    internal func startEngineIfNeeded() {
+//        guard !engineStarted else { return }
 
-       let engine = FlutterEngine(name: "fastag_engine")
-       engine.run()
-       GeneratedPluginRegistrant.register(with: engine)
-       NetworkMonitor.shared.startMonitoring()
-       flutterEngine = engine
-
-
-       // Prepare FlutterViewController internally
-       let flutterVC = FlutterViewController(engine: engine, nibName: nil, bundle: nil)
-       flutterVC.modalPresentationStyle = .fullScreen
-       flutterViewController = flutterVC
-
-       engineStarted = true
-   }
-
-}
+//        let engine = FlutterEngine(name: "fastag_engine")
+//        engine.run()
+//        GeneratedPluginRegistrant.register(with: engine)
+//        NetworkMonitor.shared.startMonitoring()
+//        flutterEngine = engine
 
 
-public final class DemoApp {
+//        // Prepare FlutterViewController internally
+//        let flutterVC = FlutterViewController(engine: engine, nibName: nil, bundle: nil)
+//        flutterVC.modalPresentationStyle = .fullScreen
+//        flutterViewController = flutterVC
 
-   public static func openSdk(
-       from rootViewController: UIViewController,
+//        engineStarted = true
+//    }
 
-   ) {
-       let engine = InternalFlutterEngine.shared
-       engine.startEngineIfNeeded()
-
-       guard let flutterVC = engine.flutterViewController else {
-           return
-       }
-       rootViewController.present(flutterVC, animated: true)
-   }
+// }
 
 
-}
+// public final class DemoApp {
+
+//    public static func openSdk(
+//        from rootViewController: UIViewController,
+
+//    ) {
+//        let engine = InternalFlutterEngine.shared
+//        engine.startEngineIfNeeded()
+
+//        guard let flutterVC = engine.flutterViewController else {
+//            return
+//        }
+//        rootViewController.present(flutterVC, animated: true)
+//    }
+
+
+// }
